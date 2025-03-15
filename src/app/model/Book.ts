c9688b452e3b5db7dd75a4a50ae7bafce1e2bd08
charuka96 <chaukathisaru96@gmail.com>
2025-03-15 23:06:30 +0530

    table updated

--- src/app/model/Book.ts
+++ src/app/model/Book.ts
@@ -1,21 +1,32 @@
-export class Book{
-  public bookId:string = "";
-  public price :number;
-  public title :string;
-  public isbn:string;
-  public category:string;
-  public authorId:number;
-  public authorName:string;
-  public updateDate:Date;
+export class Book {
+  public bookId: string = '';
+  public price: number;
+  public title: string;
+  public isbn: string;
+  public category: string;
+  public authorId: number;
+  public authorName: string;
+  public updateDate: Date;
 
-constructor(price:number, title:string, isbn:string, category:string , authorId:number , authorName:string , updateDate:Date){
-  this.price = price;
-  this.title= title;
-  this.isbn= isbn;
-  this.category= category;
-  this.authorId= authorId;
-  this.authorName= authorName;
-  this.updateDate= updateDate;
-}
+  constructor(
+    price: number,
+    title: string,
+    isbn: string,
+    category: string,
+    authorId: number,
+    authorName: string,
+    updateDate: Date
+  ) {
+    this.price = price;
+    this.title = title;
+    this.isbn = isbn;
+    this.category = category;
+    this.authorId = authorId;
+    this.authorName = authorName;
+    this.updateDate = updateDate;
+  }
 
+  public setBookId() {
+    this.bookId = this.bookId;
+  }
 }
--- src/app/page/view-book/view-book.component.ts
+++ src/app/page/view-book/view-book.component.ts
@@ -1,28 +1,46 @@
 import { CommonModule } from '@angular/common';
 import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-view-book',
   standalone: true,
-  imports: [RouterLink,CommonModule],
+  imports: [RouterLink, CommonModule, FormsModule],
   templateUrl: './view-book.component.html',
-  styleUrl: './view-book.component.css'
+  styleUrl: './view-book.component.css',
 })
 export class ViewBookComponent {
+  bookinfo: any = [];
 
-
-bookinfo:any =[];
-
-constructor(private http:HttpClient){
-  this.http.get('http://localhost:8080/book/getAll-book').subscribe(res=>{
-    console.log(res);
-    this.bookinfo = res;
-  })
-}
-
-
-
-
+  constructor(private http: HttpClient) {
+    try {
+      this.http
+        .get('http://localhost:8080/book/getAll-book')
+        .subscribe((res) => {
+          console.log(res);
+          this.bookinfo = res;
+        });
+    } catch (error) {
+      console.log(error);
+    }
+  }
+  public deleteBook(id: any) {
+    try {
+      console.log(id);
+      this.http
+        .delete(`http://localhost:8080/book/delete-book/${id}`)
+        .subscribe((res) => {});
+      alert('Book deleted');
+    } catch (error) {}
+  }
+  searchBookInfo: any = [];
+  public searchBook(authorId: any) {
+    this.http
+      .get(`http://localhost:8080/book/search-by-authorId/${authorId}`)
+      .subscribe((res) => {
+        this.searchBookInfo = res;
+      });
+  }
 }
